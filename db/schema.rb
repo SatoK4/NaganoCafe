9572c71f010d9dd2d11d791bd50a338813da0ea6
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-01 05:15:55 +0000

    Add table-orders

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_07_31_122806) do
+ActiveRecord::Schema.define(version: 2023_08_01_050548) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "item_id", null: false
@@ -24,6 +24,18 @@ ActiveRecord::Schema.define(version: 2023_07_31_122806) do
     t.string "name", null: false
     t.text "introduction", null: false
     t.integer "price", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.string "postal_code", null: false
+    t.string "address", null: false
+    t.string "name", null: false
+    t.integer "postage", null: false
+    t.integer "price", null: false
+    t.integer "payment_method", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
