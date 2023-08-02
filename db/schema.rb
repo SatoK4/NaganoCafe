e0611afd110e327e3ae0a74190cc4d0e29a6facf
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-02 08:39:06 +0000

    Delete 無駄な migrate ファイルを削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_08_01_054148) do
+ActiveRecord::Schema.define(version: 2023_08_01_051627) do
 
   create_table "cart_items", force: :cascade do |t|
     t.integer "item_id", null: false
