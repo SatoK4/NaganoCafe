c88ec7252abffd9f1eec35df413392146a1d0ce5
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-04 07:31:21 +0000

    Add public/sessions 退会済アカウントをログイン不可に

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -21,14 +22,25 @@ class Public::SessionsController < Devise::SessionsController
   def after_sign_in_path_for(resource)
     root_path
   end
-  
+
   def after_sign_out_path_for(resource)
     root_path
   end
-  # protected
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
+
+  protected
+
+  # 退会しているかを判断するメソッド
+  def customer_state
+    # 入力されたemailからアカウントを取得
+    @customer = Customer.find_by(email: params[:customer][:email])
+    # 取得できなかったらメソッドを終了
+    return if !@customer
+    # 取得した垢のパスワードが入力と一致してるかを判別
+    if @customer.valid_password?(params[:customer][:password])
+      if @customer.is_deleted == true
+        redirect_to new_customer_registration_path
+      end
+    end
+  end
 end
