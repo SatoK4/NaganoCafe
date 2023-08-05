53e33817cda67719909963dfbc187db656b75cbe
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-05 14:02:29 +0000

    Add admin/items 新規登録画面の作成(エラーのため一時中止)

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -3,11 +3,28 @@ class Admin::ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    @item.save
+    # 後ほど商品詳細ページへ
+    redirect_to about_path
   end
 
   def show
   end
 
   def edit
+  end
+
+  def update
+  end
+
+
+  private
+  def item_params
+    params.requie(:item).permit(:image, :name, :introduction)
   end
 end
