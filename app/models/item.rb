522633f549f134d67a020df50f1863c8ada7358f
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-10 13:38:47 +0000

    Add admin/items varidates を設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,11 @@ class Item < ApplicationRecord
   has_many :order_details
   
   has_one_attached :image
+  
+  validates :name        , presence: true
+  validates :introduction, presence: true
+  validates :price       , presence: true
+  validates :image       , presence: true
 
   def total_price
     tax = 1.10
