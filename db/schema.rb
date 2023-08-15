278d85bb452c321c51e47c94481fa84867eaf7fc
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-15 07:10:48 +0000

    Update db/orders 一部のカラム名を変更

--- db/schema.rb
+++ db/schema.rb
@@ -102,8 +102,8 @@ ActiveRecord::Schema.define(version: 2023_08_04_121654) do
     t.string "postal_code", null: false
     t.string "address", null: false
     t.string "name", null: false
-    t.integer "postage", null: false
-    t.integer "total_price", null: false
+    t.integer "shipping_cost", null: false
+    t.integer "total_payment", null: false
     t.integer "payment_method", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
