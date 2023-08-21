6df3426e10b453b4296f8f71a9dc5220f96cfbc5
SatoK4 <135345700+SatoK4@users.noreply.github.com>
2023-08-21 06:33:47 +0000

    Add 注文情報確認画面を作成

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -8,6 +8,38 @@ class Public::OrdersController < ApplicationController
   end
 
   def show
-    @order = Order.find[params:id]
+    @order = Order.find(params[:id])
+  end
+
+  def confirm
+    @order = Order.new(order_params)
+    @cart_items = current_customer.cart_items.all
+    @total_price = 0
+  end
+
+  def create
+    cart_items = current_customer.cart_items.all
+    @order = current_customer.orders.new(order_params)
+    @order.shipping_cost = 800
+    if @order.save
+      cart_items.each do |cart_item|
+        order_detail = OrderDetail.new
+        order_detail.item_id = cart_item.item_id
+        order_detail.order_id = @order.id
+        order_detail.amount = cart_item.amount
+        order_detail.price = cart_item.item.price
+        order_detail.save
+      end
+      redirect_to complete_orders_path
+      cart_items.destroy_all
+    else
+      @order = Order.new(order_params)
+      render :new
+    end
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:payment_method, :postal_code, :address, :name, :image, :total_payment)
   end
 end
